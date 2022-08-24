3f4a06a303a9017283c44d5433a8b0379ad59528
Ronak Tandon <ronakt@mobiquityinc.com>
2022-08-24 18:46:27 +0530

    issue-1007 : Bug fixes in src folder
    Bug fixes applied in the div container pratigya added to the blog

---
+++ f0rsaken.tsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+const paperRex = () => {
+    // PRATIGYA
+    
+  return (
+    <div>Bharat hamara desh hai.
+        Hum sab bharatwasi bhai bhai hai.
+        Humein hamra desh prano se bhi pyara hai.
+        Iski samriddhi aur vividh sanskriti pr humein garv hai.
+        Hum iske suyogya adhikaari banne ka prayog sada karte rahenge.
+        Hum apne mata ,pita ,shikshako aur gurujano ka aadar karenge aur sabke saath shishta ka vyavahaar karenge.
+        Hum apne desh aur desh vasiyon k prati vafadaar rehne ki pratigya karte hai.
+        Unke kalyaan aur samriddhi me hi hamara sukh nihit hai.
+        | Jai Hind |
+        </div>
+  )
+}
+
+export default paperRex
